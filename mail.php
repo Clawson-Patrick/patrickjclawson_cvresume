cbbf0ea6e249d018f8cc37682c691b7b07db35b4
Patrick Clawson <patrick.clawson@west-mec.org>
2018-11-13 18:11:24 -0700

    Updated mail.php
    Changed email so I am the recipient

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "basictheme400@gmail.com";
+        $recipient = "patrick.clawson83@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
